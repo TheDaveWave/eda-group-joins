682bf09b00100d02ebceb10eb3bec1abfd21405b
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-04 11:00:33 -0500

    q2 and q3 done

--- joins-solution.sql
+++ joins-solution.sql
@@ -4,9 +4,19 @@ array_agg("addresses"."street" ||', '|| "addresses"."city" ||', '|| "addresses".
 AS "address"
 FROM "addresses" JOIN "customers" ON "addresses"."customer_id" = "customers"."id"
 GROUP BY "customers"."id";
--- 2. Get all orders and their line items (orders, quantity and product).
+
+-- 2. Get all orders and their line items (orders, quantity and products).
+SELECT "orders"."id" AS "order_id", "line_items"."quantity", "products"."description"
+FROM "orders"
+JOIN "line_items" ON "orders"."id" = "line_items"."order_id"
+JOIN "products" ON "products"."id" = "line_items"."product_id";
 
 -- 3. Which warehouses have cheetos?
+SELECT "warehouse"."warehouse"
+FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse"."id" = "warehouse_product"."warehouse_id"
+JOIN "products" ON "warehouse_product"."product_id" = "products"."id"
+WHERE "products"."description" = 'cheetos';
 
 -- 4. Which warehouses have diet pepsi?
 
