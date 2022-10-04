afdba9ce5bae3a410f8f1b789acfa1c8f25feca3
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-04 13:25:14 -0500

    q7 complete

--- joins-solution.sql
+++ joins-solution.sql
@@ -37,6 +37,7 @@ GROUP BY "a"."customer_id";
 SELECT count("customers"."id") FROM "customers";
 
 -- 7. How many products do we carry?
+SELECT count(DISTINCT "products"."description") FROM "products";
 
 -- 8. What is the total available on-hand quantity of diet pepsi?
 
